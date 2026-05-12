7f12295d9bf2ac046681fcdfec176186af864a3b
Paolo <paol.munozm@duocuc.cl>
2026-05-11 23:16:49 -0400

    Se agrego el paquete CONFIG para la API

---
+++ Moviary/Moviesdemo/src/main/java/com/example/Moviesdemo/Config/WebClientConfig.java
@@ -0,0 +1,15 @@
+package com.example.Moviesdemo.Config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient tmdbWebClient(@Value("${tmdb.base-url}") String baseUrl) {
+        return WebClient.builder().baseUrl(baseUrl).build();
+    }
+}
